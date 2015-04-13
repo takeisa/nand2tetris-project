2f7e78e39be04099b1f9333bf0bb7bf6f9bc5fd8
Satoshi Takei <s.takei.dev@gmail.com>
2015-04-13 21:39:41 +0900

    Implement Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	@R1
+	D=M
+	@R3
+	M=D
+	@0
+	D=A
+	@R2
+	M=D
+(LOOP)
+	@R3
+	D=M
+	@END
+	D;JEQ
+
+	@R0
+	D=M
+	@R2
+	M=D+M
+	
+	@R3
+	M=M-1
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
+
